6d6db1255e052819902fc0c83b915afcf016d7ac
codexdevelop <tryoutbothai@gmail.com>
2025-03-20 09:41:40 +0530

    Update render_template.py

--- Beniwal/util/render_template.py
+++ Beniwal/util/render_template.py
@@ -1,6 +1,6 @@
 import jinja2
 from info import *
-from Beniwal.bot import TechVJBot
+from Beniwal.bot import BeniwalBot
 from Beniwal.util.human_readable import humanbytes
 from Beniwal.util.file_properties import get_file_ids
 from Beniwal.server.exceptions import InvalidHash
@@ -10,8 +10,8 @@ import aiohttp
 
 
 async def render_page(id, secure_hash, src=None):
-    file = await TechVJBot.get_messages(int(LOG_CHANNEL), int(id))
-    file_data = await get_file_ids(TechVJBot, int(LOG_CHANNEL), int(id))
+    file = await BeniwalBot.get_messages(int(LOG_CHANNEL), int(id))
+    file_data = await get_file_ids(BeniwalBot, int(LOG_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
         logging.debug(f"Invalid hash for message with - ID {id}")
@@ -25,9 +25,9 @@ async def render_page(id, secure_hash, src=None):
     tag = file_data.mime_type.split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
     if tag in ["video", "audio"]:
-        template_file = "TechVJ/template/req.html"
+        template_file = "Beniwal/template/req.html"
     else:
-        template_file = "TechVJ/template/dl.html"
+        template_file = "Beniwal/template/dl.html"
         async with aiohttp.ClientSession() as s:
             async with s.get(src) as u:
                 file_size = humanbytes(int(u.headers.get("Content-Length")))
