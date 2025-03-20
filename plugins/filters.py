36f1aef1d3440b8ff124e99c029de793097384bc
codexdevelop <tryoutbothai@gmail.com>
2025-03-20 18:49:54 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Codexownerr
+# Join Update Channel For Amazing Bot @codexbotmaker
+# Ask Doubt on telegram @codexdisscus
 
 import io
 from pyrogram import filters, Client, enums
