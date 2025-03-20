563bebbe03e717f1879fde1cf1fbe0f381baf2e4
codexdevelop <tryoutbothai@gmail.com>
2025-03-20 19:04:02 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,17 +1,17 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Codexownerr
+# Join Update Channel For Amazing Bot @codexbotmaker
+# Ask Doubt on telegram @codexdisscus
 
 import re, math, logging, secrets, mimetypes, time
 from info import *
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
-from TechVJ.bot import multi_clients, work_loads, TechVJBot
-from TechVJ.server.exceptions import FIleNotFound, InvalidHash
-from TechVJ import StartTime, __version__
-from TechVJ.util.custom_dl import ByteStreamer
-from TechVJ.util.time_format import get_readable_time
-from TechVJ.util.render_template import render_page
+from Beniwal.bot import multi_clients, work_loads, BeniwalBot
+from Beniwal.server.exceptions import FIleNotFound, InvalidHash
+from Beniwal import StartTime, __version__
+from Beniwal.util.custom_dl import ByteStreamer
+from Beniwal.util.time_format import get_readable_time
+from Beniwal.util.render_template import render_page
 
 routes = web.RouteTableDef()
 
