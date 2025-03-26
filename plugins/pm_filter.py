24f44ed203c3004608f7662cf3e5f98fdf6c99aa
codexdevelop <tryoutbothai@gmail.com>
2025-03-26 11:44:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1983,6 +1983,24 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
+    elif query.data == "instagram":
+        buttons = [[
+            InlineKeyboardButton('📷 Open Instagram', url="https://instagram.com")
+        ],[
+            InlineKeyboardButton('🔙 Back', callback_data='start'),
+            InlineKeyboardButton('❌ Close', callback_data='close_data')
+        ]]
+        await client.edit_message_media(
+            query.message.chat.id, 
+            query.message.id, 
+            InputMediaPhoto(random.choice(PICS))  # अगर कोई Instagram image दिखानी हो
+        )
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.INSTAGRAM_TXT,  # यहाँ script.py में जो text define किया है वो आएगा
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
     elif query.data == "subscription":
         buttons = [[
             InlineKeyboardButton('⇚Back', callback_data='start')
