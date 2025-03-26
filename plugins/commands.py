0ab7cee302d6b03951cb737d60f7657feae52a7e
codexdevelop <tryoutbothai@gmail.com>
2025-03-26 09:01:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -638,7 +638,11 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(btn)
             )
             return
-
+    if STREAM_MODE == True:
+        button = [[InlineKeyboardButton('sᴛʀᴇᴀᴍ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ', callback_data=f'generate_stream_link:{file_id}')]]
+        reply_markup=InlineKeyboardMarkup(button)
+    else:
+        reply_markup = None
     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
